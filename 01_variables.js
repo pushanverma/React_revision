31ce12001fbab644e7710d9320ee449f87993368
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2025-04-15 18:56:16 +0530

    adding js code

---
+++ 01_variables.js
@@ -0,0 +1,42 @@
+const accountId =14453;
+
+let accountEmail ="pushanverma.99@gmail.com";
+
+accountCity ="Delhi"
+
+let accountState;
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountCity,accountState]);
+
+
+// let , var and const 
+
+
+// var is used in old js version , where it had a problem of scope , it can be changed anywhere in the code which 
+// introduced the problem of scoping 
+
+// where let came in picture and let is block scoped , 
+
+// we can store any type of value in (let and var ) since javsScript is dynamically typed language 
+
+// whereas const is a type of variable which is fixed , its value cannot be changed , it is also blocked scoped.a
+
+// Note : if we dont write any variable before variableName , js automatically initializes memory to it 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
