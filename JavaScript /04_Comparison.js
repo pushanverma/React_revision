6932f0da08f4deb1f55de5cf8961356fdafd6297
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2025-05-17 14:26:03 +0530

    comparison code added

---
+++ JavaScript /04_Comparison.js
@@ -0,0 +1,45 @@
+//topic: Basic comparision (with same dataTypes)
+
+// console.log(2>1);
+// console.log(2==2);
+// console.log(2<3);
+// console.log(2!=1);
+
+//topic: Comparison with diffrent Data Types
+
+//comparing string number  with original  number
+// console.log("2" > 1);
+// console.log("02" < 1);
+
+//comparing string alphabets with original number
+
+// console.log("abc" > 1);
+
+
+// console.log("true"==true);
+
+// In js , comparison operators(< , > , <=, >= ) and strict equality(===) behaves diffrently 
+
+//topic: comparing null 
+
+console.log(null<0);
+console.log(null<=0);
+console.log(null>0);
+console.log(null>=0);
+console.log(null===0);
+console.log(null!=0);
+
+
+//topic:comparing undefiend
+console.log(undefined<0);
+console.log(undefined<=0);
+console.log(undefined>0);
+console.log(undefined>=0);
+console.log(undefined===0);
+console.log(undefined!=0);
+
+
+
+
+
+
