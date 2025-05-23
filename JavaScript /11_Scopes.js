335a9c21682ec9013d600d955fc43cd9b1c195bc
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2025-05-23 16:09:37 +0530

    comment extended

--- JavaScript /11_Scopes.js
+++ JavaScript /11_Scopes.js
@@ -57,12 +57,10 @@ console.log(addOne(5), "...addOne call placed after function ");
 //2.Storing in variable(because variables can hold functions)
 
 // console.log(addTwo(2), "....addTwo call placed before function ");
-// the above statement cannot take place because we cannot use a variable before it is initialized, it is like using something which has not produced uptill now
+// the above statement cannot take place because we cannot use a variable before it is initialized, it is like using something which has not produced uptill now, this concept is called hoisting when you cannot access variable before declaring that
 
 const addTwo = function (num) {
   return num + 2;
 };
 
 console.log(addTwo(2), "....addTwo call placed after function ");
-
-
