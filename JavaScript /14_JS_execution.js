53b19e4c08678bc323eb9e4fbbce61121c8de863
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2025-06-04 22:36:04 +0530

    js execution

---
+++ JavaScript /14_JS_execution.js
@@ -0,0 +1,14 @@
+let val1 =10;
+let val2=15;
+
+function addNumber(val1, val2){
+    let result =val1+val2;
+    return result;
+}
+
+let result1=addNumber(val1,val2);
+let result2=addNumber(50,60);
+
+console.log(result1);
+console.log(result2);
+
