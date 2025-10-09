629da72567619d1c318911e865b4ca78150c756f
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2025-10-10 00:54:56 +0530

    truthy and falsy code added

--- JavaScript /02_dataTypes.js
+++ JavaScript /02_dataTypes.js
@@ -12,6 +12,36 @@ console.log(typeof null);
 console.log(typeof undefined);
 
 
+//Truthy and Falsy Values (can be checked by Boolean method )
+
+
+//Numbers 
+console.log(Boolean(1),"....+ve value");
+console.log(Boolean(-1),"...-ve value");
+console.log(Boolean(0),"....0 value");   //false
+console.log(Boolean(0.000001),".....+ve decimal number value");
+console.log(Boolean(-0.000001),".....+ve decimal number value");
+console.log(Boolean(Infinity),"....+ve infinity Value");
+console.log(Boolean(-Infinity),"...-ve infinity value");
+console.log(Boolean(NaN),"....not a Number(NaN)");   //false
+
+console.log("----------------------------------------------");
+
+
+//String 
+console.log(Boolean(''),"...empty String");// false
+console.log(Boolean(" "),"....space(looking like empty string)");
+
+console.log("------------------------------------------------");
+
+//undefined
+console.log(Boolean(undefined),"....undefined"); //false
+console.log(Boolean(null),"....null");  //false
+
+
+
+
+
 
 
 
